03be0adc655bb4a8a4744e813a1e8ad89a45da51
MitchellWoodUK <80631581+MitchellWoodUK@users.noreply.github.com>
2022-01-13 15:40:08 +0000

    Adding UserRolesView

---
+++ OnlineShop2022/Areas/Admin/Controllers/UserRolesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop2022.Models;
+using System.Threading.Tasks;
+
+namespace OnlineShop2022.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserRolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<CustomUserModel> _userManager;
+        public UserRolesController(RoleManager<IdentityRole> roleManager, UserManager<CustomUserModel> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineShop2022/Areas/Admin/Models/UserRolesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop2022.Models;
+using System.Collections.Generic;
+
+namespace OnlineShop2022.Areas.Admin.Models
+{
+    public class UserRolesViewModel
+    {
+        public CustomUserModel User { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
